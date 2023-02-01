e312e803ec3ec89118ef436b77fe7de1fd2f6cbc
Joshua Gutz <jgutz87@gmail.com>
2023-01-31 20:19:41 -0600

    trying to make location a variable

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import weather_app
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    location = "SanAntonio,TX"
-    weather = weather_app.get_weather(location)
-    return render_template("weather.html", location=location, weather=weather)
+    location = None
+    weather = None
+    error = None
+    if request.method == "POST":
+        location = request.form.get("location")
+        try:
+            weather = weather_app.get_weather(location)
+        except Exception as e:
+            error = "Sorry, no data available at this time"
+    return render_template("weather.html", location=location, weather=weather, error=error)
 
 if __name__ == "__main__":
     app.run()
