521eba9e5a224de29011c848625c02c850dba199
Joshua Gutz <jgutz87@gmail.com>
2023-01-31 21:23:16 -0600

    modified: app.py, weather.html combined to a single route endpoint

--- app.py
+++ app.py
@@ -3,24 +3,19 @@ import weather_app
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     location = None
     weather = None
     error = None
-    return render_template("weather.html")
 
-@app.route("/weather", methods=["POST"])
-def weather():
-    location = None
-    weather = None
-    error = None
-    location = request.form["location"]
-    weather = weather_app.get_weather(location)
-    if weather:
-        return render_template("weather.html", location=location, weather=weather)
-    else:
-        return render_template("weather.html", error="Sorry, no data available at this time")
+    if request.method == "POST":
+        location = request.form["location"]
+        weather = weather_app.get_weather(location)
+        if not weather:
+            error = "Sorry, no data available at this time"
+
+    return render_template("weather.html", location=location, weather=weather, error=error)
 
 if __name__ == "__main__":
     app.run()
